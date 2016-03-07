a1f1b40a70e1a9269c89c9f288d981c4abe47325
Henry Chen <yuhsinchen19940823@gmail.com>
2016-03-07 15:15:10 -0500

    initial commit

---
+++ src/main/java/edu/emory/mathcs/nlp/character_mining/App.java
@@ -0,0 +1,13 @@
+package edu.emory.mathcs.nlp.character_mining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
